9bd18fe0b21dcd5dc60a47b304329ea06d4a7a1b
Tanya Rai <98468161+tanyarai1404@users.noreply.github.com>
2022-06-02 14:37:14 +0000

    Question 15 updated

--- script.js
+++ script.js
@@ -245,3 +245,53 @@ console.log(asc(arr));
 number(5);
 */
 
+
+//Question 15
+/*
+function evaluatePostfix(exp)
+{
+	
+		let stack=[];
+		
+		
+		for(let i=0;i<exp.length;i++)
+		{
+			let c=exp[i];
+			
+			
+			if(! isNaN( parseInt(c) ))
+			stack.push(c.charCodeAt(0) - '0'.charCodeAt(0));
+			
+      else
+			{
+				let val1 = stack.pop();
+				let val2 = stack.pop();
+				
+				switch(c)
+				{
+					case '+':
+					stack.push(val2+val1);
+					break;
+					
+					case '-':
+					stack.push(val2- val1);
+					break;
+					
+					case '/':
+					stack.push(val2/val1);
+					break;
+					
+					case '*':
+					stack.push(val2*val1);
+					break;
+			}
+			}
+		}
+		return stack.pop();
+}
+
+let exp="531*+9-";
+console.log("postfix evaluation: "+evaluatePostfix(exp));
+*/
+
+
